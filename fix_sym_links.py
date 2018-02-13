e19c70e22a76abf612b728a12fd85569d9a41338
Ian Humphrey <ihumphrey@usgs.gov>
2018-02-13 08:53:07 -0700

    updated fix_sym_links.py to properly update sym-linked binaries in v00X area

--- fix_sym_links.py
+++ fix_sym_links.py
@@ -4,21 +4,39 @@ import sys
 
 def process_files(files):
     DEBUG = True
+
     for f in files:
+        # Grab executables (no extensions)
         executable, ext = os.path.splitext(f)
+
         # Only work on binary (executable) files
         if ext == "":
             print "Processing %s" % executable
+
             ls_cmd = ["ls", "-l", executable]
             ls_cmd_output = subprocess.check_output(ls_cmd)
             if DEBUG:
                 print "\tls cmd output: {}".format(ls_cmd_output)
+
             sym_path = ls_cmd_output.split("->")
-            # TODO: use the 2nd arg of split to get the path (and then remove the trailing \n)
-            # TODO: run the ln commands needed.
-            print "" 
-            print "\tsym path: {}".format(sym_path)
-            print ""
+            sym_target = sym_path[1]
+            if DEBUG:
+                print "\tsplit[1]: {}".format(sym_target)
+
+            rm_cmd = ["rm", executable]
+            if DEBUG:
+                rm_cmd.insert(0, "echo")
+            rm_cmd_output = subprocess.check_call(rm_cmd)
+            if DEBUG:
+                print "\trm cmd: {}".format(rm_cmd_output)
+
+            sym_cmd = ["ln", "-s", sym_target, executable]
+            if DEBUG:
+                sym_cmd.insert(0, "echo")
+            sym_cmd_output = subprocess.check_output(sym_cmd)
+            if DEBUG:
+                print "\tsym cmd: {}".format(sym_cmd_output)
+
             print "-"*80
             print ""
 
