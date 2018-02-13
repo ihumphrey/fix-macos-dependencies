f57b275ae774a5a11ac65977049ee168f380514b
Ian Humphrey <ihumphrey@usgs.gov>
2018-02-13 10:58:45 -0700

    updated fix_binary_paths_recursively.py to modify binary dependencies correctly

--- fix_binary_paths_recusively.py
+++ fix_binary_paths_recusively.py
@@ -9,6 +9,8 @@ path = sys.argv[1]
 
 print "path:  {}".format(path)
 for root, dirs, files in os.walk(path):
+    if "macports/sources" in root:
+        continue
     if VERBOSE:
         print "root:  {}".format(root)
         print "dirs:  {}".format(dirs)
@@ -16,11 +18,19 @@ for root, dirs, files in os.walk(path):
 
     for f in files:
         executable, ext = os.path.splitext(f)
+        executable = os.path.join(root, executable)
+
         # Check to see that the file is a binary and is not a symlink
         if ext == "" and not os.path.islink(executable):
-            print "Processing {}...".format(executable)
+            print "Processing {} ...".format(executable)
             otool_L_cmd = ["otool", "-L", executable]
-            otool_L_cmd_output = subprocess.check_output(otool_L_cmd)
+            try:
+                otool_L_cmd_output = subprocess.check_output(otool_L_cmd)
+            except subprocess.CalledProcessError as e:
+                print "\tWARNING -- skipping {}".format(executable)
+                print "\t\tCMD: {}".format(otool_L_cmd)
+                print "\t\tMSG: {}".format(repr(e))
+                continue
             if VERBOSE:
                 print "otool -L : {}".format(otool_L_cmd_output)
 
@@ -28,7 +38,7 @@ for root, dirs, files in os.walk(path):
             v00x_dependencies = otool_L_cmd_output.splitlines()
             old_v00x = "v006"
             new_v00x = "v007"
-            for dependency in v00X_dependencies:
+            for dependency in v00x_dependencies:
                 # Replace old v00x with new v00x
                 if old_v00x in dependency:
                     # Format of otool -L: dependency (extraneous info)
@@ -43,10 +53,11 @@ for root, dirs, files in os.walk(path):
 
                     install_name_tool_cmd_output = subprocess.check_output(install_name_tool_cmd)
                     if DEBUG:
-                        print "\tinstall_name_tool cmd: {}".format(install_name_tool_cmd_ouput)
+                        print "\tinstall_name_tool cmd: {}".format(install_name_tool_cmd_output)
 
-                    otool_L_cmd_output = subprocess.check_output(otool_L_cmd)
-                    print "otool -L: {}".format(otool_L_cmd_output)
+                    if VERBOSE:
+                        otool_L_cmd_output = subprocess.check_output(otool_L_cmd)
+                        print "otool -L: {}".format(otool_L_cmd_output)
             print ""
             print "-"*80
             print ""
